98e5074277b7226a300e703d5d732148303eb16d
HARIKSREEE <sivan.hari.sr@gmail.com>
2018-12-05 17:58:29 +0530

    added product.json

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -11,5 +11,22 @@ import { Component } from '@angular/core';
 export class ProductListComponent {
 
     pageTitle: string = "Product List";
+    products: any[] = [
+
+        {
+            "productId": 2,
+            "productName": "Garden Cart",
+            "productCode": "GDN-0023",
+            "releaseDate": "March 18, 2018",
+            "description": "",
+            "price": "32.99",
+            "starRating": "4.2",
+            "imageUrl": ""
+        },
+        {
+            
+        }
+
+    ];
 
 }
